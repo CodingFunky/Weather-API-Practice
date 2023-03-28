958930d7e3811812bcba32dd197de9222c7ba076
CodingFunky <ryan.namingishard2@gmail.com>
2023-03-27 20:29:50 -0600

    shows forecast

--- app.js
+++ app.js
@@ -25,11 +25,12 @@ function getWeather(location) {
       return response.json();
     })
     .then(function (response) {
+      console.log('getWeather response');
       console.log(response);
       printWeather(response, location);
     })
     .then(function (response) {
-      getForecast(selectStateForm.value);
+      getForecast(location);
     })
     .catch(function (err) {
       console.log('error')
@@ -42,8 +43,23 @@ function getForecast(location) {
       return response.json();
     })
     .then(function (response) {
-      console.log(response);
-      printForecast(response, location);
+      console.log('New List: ')
+      console.log(response)
+      const list = response.list.filter(item => {
+        const date = new Date(item.dt * 1000);
+        const today = new Date();
+        // ********** WHAT IS THIS??? **********
+        // return date.getDate() === today.getDate() + 1 || 
+        //      date.getDate() === today.getDate() + 2 || 
+        //      date.getDate() === today.getDate() + 3 || 
+        //      date.getDate() === today.getDate() + 4 || 
+        //      date.getDate() === today.getDate() + 5 || 
+        //      date.getDate() === today.getDate() + 6 || 
+        //      date.getDate() === today.getDate() + 7 || 
+        //      date.getDate() === today.getDate() + 8;
+        return date.getDate()
+      });
+      printForecast(response, list);
     })
 }
 printWeather = function (data, location) {
@@ -58,37 +74,56 @@ printWeather = function (data, location) {
   const dateString = formatter.format(dateObj); // Convert to readable string
   document.querySelector('#lastUpdatedInfo').textContent = dateString
 }
-printForecast = function (data, location) {
-  for (let i = 0; i <= 7; i++) {
+printForecast = function (data, list) {
+  const forecastBody = document.querySelector('#forecastBody')
+  while (forecastBody.firstChild) {
+    forecastBody.removeChild(forecastBody.firstChild);
+  }
+  for (let i = 0; i <= 5; i++) {
+    // make card
     const forecastCard = document.createElement('div');
     forecastCard.classList = 'forecastCard';
+
+    // get cloud info
     const forecastClouds = document.createElement("img");
     forecastClouds.classList = 'forecast-cloud'
     forecastClouds.src = `https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png`
-    const forecastElement = document.createElement("div");
-    forecastElement.classList = "forecast-element";
-    // forecastElement.innerHTML = data.list[i]
-    forecastElement.appendChild(forecastClouds);
+
+    // get temp info
     const forecastTemp = document.createElement("div");
     forecastTemp.classList = 'forecast-temp';
     forecastTemp.innerHTML = Math.round((data.list[i].main.temp * 9 / 5) - 459.67) + '°F'
+
+    // get day of the week
     const dayofweekEle = document.createElement("div");
     dayofweekEle.classList = 'dayofweek';
-    const timestamp2 = data.list[i].dt
-    console.log(timestamp2)
-    // const timestamp = data.list[i].dt;
-    // const dateObj = new Date(timestamp * 1000);
-    // const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dateObj.getUTCDay()];
-    // console.log('Date:', dateObj.toISOString().slice(0, 10));
-    // console.log('Day of the week:', dayOfWeek);
-    const timestamp = 1679637600;
-    const dateObj = new Date(timestamp * 1000);
-    const dateStr = dateObj.toUTCString();
-    console.log(dateStr);
-    dayofweekEle.innerHTML = dateStr
+
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    // generate list
+    const today = new Date();
+    document.querySelector('#dayName').innerHTML = daysOfWeek[today.getDay()]
+    const forecastList = []
+    for (let i = 0; i < 7; i++) {
+      const date = new Date()
+      date.setDate(today.getDate() + i);
+  
+      const dayOfWeek = daysOfWeek[date.getDay()];
+  
+      if (dayOfWeek !== list[list.length - 1].dayOfWeek && dayOfWeek !== daysOfWeek[today.getDay()]) {
+        forecastList.push(dayOfWeek);
+      }
+    }
+    dayofweekEle.innerHTML = forecastList[i]
+
+    // append created elements to card and then card to HTML element
     forecastCard.append(forecastTemp, forecastClouds, dayofweekEle)
     document.querySelector('#forecastBody').appendChild(forecastCard);
   }
 }
+
+// makeForecastCard = function (data) {
+
+// }
 
 getWeather(selectStateForm.value);
